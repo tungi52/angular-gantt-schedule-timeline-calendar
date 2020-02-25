17dcb73e268883ca15c50c8f4f799afc48cb337f
tungi52 <tungi52@gmail.com>
2020-02-25 21:33:54 +0100

    Update angular-gantt-schedule-timeline-calendar.component.ts

--- projects/angular-gantt-schedule-timeline-calendar/src/lib/angular-gantt-schedule-timeline-calendar.component.ts
+++ projects/angular-gantt-schedule-timeline-calendar/src/lib/angular-gantt-schedule-timeline-calendar.component.ts
@@ -22,27 +22,55 @@ import GSTC from "gantt-schedule-timeline-calendar/dist/index.esm.js";
   encapsulation: ViewEncapsulation.None
 })
 export class GSTCComponent implements AfterViewInit, OnDestroy {
-  @Input() config: any;
-  @Output() onState: EventEmitter<any> = new EventEmitter();
-  @ViewChild("gstcElement", { static: true }) gstcElement: ElementRef;
+  private GSTC: any;
+  private state: any;
+  private _config: any;
 
-  GSTC: any;
-  state: any;
+  @Input()
+  public get config(): any {
+    return this._config;
+  }
+
+  public set config(value: any) {
+    this._config = value;
+
+    if (this.GSTC) {
+      let newConfig = GSTC.api.stateFromConfig(this.config).data.config;
+      this.GSTC.app.vidoInstance.state.update("config", newConfig);
+      this.GSTC.app.update();
+    } else {
+      this.createApp();
+    }
+  }
+
+  @Output() onState: EventEmitter<any> = new EventEmitter();
+
+  @ViewChild("gstcElement", { static: true }) gstcElement: ElementRef;
 
   constructor() {}
 
   ngAfterViewInit() {
-    const element = this.gstcElement.nativeElement;
-    this.state = GSTC.api.stateFromConfig(this.config);
-    this.onState.emit(this.state);
-    this.GSTC = GSTC({
-      element,
-      state: this.state
-    });
+    this.createApp();
   }
 
   ngOnDestroy() {
-    this.state.destroy();
+    this.destroyApp();
+  }
+
+  private createApp() {
+    if (this._config) {
+      const element = this.gstcElement.nativeElement;
+      this.state = GSTC.api.stateFromConfig(this.config);
+      this.onState.emit(this.state);
+      this.GSTC = GSTC({
+        element,
+        state: this.state
+      });
+    }
+  }
+
+  private destroyApp() {
     this.GSTC.app.destroy();
+    this.state.destroy();
   }
 }
